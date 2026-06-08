bd0cca9912a11307a976ad7600754c5dcd3f94ec
AliMMCR <ali@mmcrealestate.com>
2026-06-07 18:09:24 -0700

    structured test call request

--- adzuna api/adzuna_tracked_locations.py
+++ adzuna api/adzuna_tracked_locations.py
@@ -2,13 +2,35 @@ import os
 import time
 import requests
 from urllib.parse import quote
+from dotenv import load_dotenv
 
-app_id = os.getenv("app_id")
-app_key = os.getenv("app_key")
+load_dotenv()
 
-endpoint_url = ""
+app_id = os.getenv("adzuna_app_id")
+app_key = os.getenv("adzuna_app_key")
 
-ai_keywords = ['"artificial intelligence"', '"machine learning"', '"large language model"', '"LLM"',
-              '"RAG"', '"LangChain"', '"GenAI"', '"generative AI"']
+endpoint_url = "https://api.adzuna.com/v1/api/jobs/us/search/1?"
 
-locations = {"San Francisco"}
+test_location = "San Francisco, CA"
+
+# Pull request limits: 2.5k a month, 1k per week, 250 per day, 25 per minute
+params={
+        "app_id": app_id,
+        "app_key": app_key,
+        "what": "machine learning",
+        "where": "San Francisco",
+        "results_per_page": 25,
+        "content-type": "application/json"
+    }
+
+response = requests.get(endpoint_url, params=params, timeout=30)
+print("status:", response.status_code)
+print("URL:", response.url)
+
+response.raise_for_status()
+data = response.json()
+
+print("Total job count:", data.get("count"))
+
+
+
